548c40ed766d69dd530414711413571d37acd4f5
Xiao Du <xiaodu910@gmail.com>
2018-01-23 23:30:33 -0800

    Chapter 2's exercises

---
+++ chapter_2.py
@@ -0,0 +1,43 @@
+#2-3
+print "2-3_---------------------------------"
+name = "Eric"
+message = "Hello " + name + ", would you like to learn some Python today?"
+
+print message
+
+#2-4
+print "2-4_---------------------------------"
+name2 = "Alex"
+
+print name2 + " in titlecase: " + name2.title()
+print name2 + " in uppercase: " + name2.upper()
+print name2 + " in lowercase: " + name2.lower()
+
+#2-5
+print "2-5_---------------------------------"
+print "Albert Einstein once said, \"A person who never made a mistake never tried anything new.\""
+
+#2-6
+print "2-6_---------------------------------"
+famous_person = "Albert Einstein"
+print famous_person + " once said, \"A person who never made a mistake never tried anything new.\""
+
+#2-7
+print "2-7_---------------------------------"
+name3 = "   Olivia   "
+print name3
+print "\t" + name3.lstrip()
+print name3.rstrip()
+print "\n" + name3.strip()
+
+#2-8
+print "2-8_---------------------------------"
+print 2 + 6
+print 2 * 4
+print 10 - 2
+print 16 / 2
+
+#2-9
+print "2-9_---------------------------------"
+favorite_number = 6
+print "my favorite number is " + str(favorite_number)
